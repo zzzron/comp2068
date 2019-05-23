3736349381d4b1beb7d80505b03ee6d274bea021
zzzron <zzzron135@gmail.com>
2019-05-23 16:51:14 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -11,4 +11,5 @@ app.get('/about', (req, res) => {
     res.send(`I like long walks on the beach.`);
 });
 
-app.listen(4000, () => console.log('Listening on 4000'));
+const port = (process.env.PORT || 4000);
+app.listen(port, () => console.log(`Listening on ${port}`));
